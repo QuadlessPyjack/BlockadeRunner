d524bf02a128ab4c6f17e54e8ea416019aaf340f
Bogdan Vitel <bogdan.vitel@outlook.com>
2017-01-21 19:43:33 +0200

    Added choo-choo model, updated smooth zoom scroll, began implementation of map generator

---
+++ Assets/Scripts/Gaia/MapGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using MadMilkman.Ini;
+
+namespace Assets.Scripts.Gaia
+{
+    class MapGenerator : MonoBehaviour
+    {
+        public Vector2 m_mapSize = new Vector2(100, 100);
+        public List<string> m_supportedBiomes = new List<string>{ "34324", "54545" };
+
+        private string BIOME_SETTINGS_PATH       = "//Config//BiomeSettings.ini";
+        private string DECORATIONS_SETTINGS_PATH = "//Config//Decorations.ini";
+
+        private IniOptions m_iniOptions = new IniOptions();
+        private IniFile m_biomeSettingsINI = new IniFile();
+        private IniFile m_decorationsSettingsINI = new IniFile();
+
+        private void initSettings()
+        {
+            try
+            {
+                m_biomeSettingsINI.Load(Application.dataPath + BIOME_SETTINGS_PATH);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("[Gaia] Biome settings file not found!");
+                Debug.LogError(e.Message);
+                throw;
+            }
+            try
+            {
+                m_biomeSettingsINI.Load(Application.dataPath + BIOME_SETTINGS_PATH);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("[Gaia] Decorations settings file not found!");
+                Debug.LogError(e.Message);
+                m_decorationsSettingsINI.Load(DECORATIONS_SETTINGS_PATH);
+                throw;
+            }
+        }
+
+        private void Start()
+        {
+            initSettings();
+
+        }
+    }
+}
--- Assets/Scripts/ZoomSmooth.cs
+++ Assets/Scripts/ZoomSmooth.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ZoomSmooth : MonoBehaviour {
 
-	public  float zoomStep = 1.0f;
+	public  float zoomStep = 0.2f;
+    public float zoomCapMin = 1.0f;
+    public float zoomCapMax = 10.0f;
 	private float zoomAmount = 0.0f; 
 
 	// Use this for initialization
@@ -19,14 +19,17 @@ public class ZoomSmooth : MonoBehaviour {
 
 		if (Input.GetAxis("Mouse ScrollWheel") > 0)
 		{
-			zoomAmount += zoomStep * Time.deltaTime;
+			zoomAmount += zoomStep * (1 / Time.deltaTime);
 		}
 
 		if (Input.GetAxis("Mouse ScrollWheel") < 0)
 		{
-			zoomAmount -= zoomStep * Time.deltaTime;
+			zoomAmount -= zoomStep * (1 / Time.deltaTime);
 		}
 
-		Camera.main.orthographicSize = Vector2.Lerp(new Vector2(Camera.main.orthographicSize, 0.0f), new Vector2(zoomAmount, 0.0f), 10).x;
-	}
+		float ortographicSize = Vector2.Lerp(new Vector2(Camera.main.orthographicSize, 0.0f), new Vector2(zoomAmount, 0.0f), zoomStep).x;
+        ortographicSize = Mathf.Clamp(ortographicSize, zoomCapMin, zoomCapMax);
+        Camera.main.orthographicSize = ortographicSize;
+
+    }
 }
