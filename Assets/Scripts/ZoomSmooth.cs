ee4de46a48b34c5e6ba10096f8dd00f71e1bfb4d
Bogdan Vitel <bogdan.vitel@outlook.com>
2017-01-21 14:28:35 +0200

    Added: base project files, isometric camera setup, simple mouse zoom script for camera

---
+++ Assets/Scripts/ZoomSmooth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomSmooth : MonoBehaviour {
+
+	public  float zoomStep = 1.0f;
+	private float zoomAmount = 0.0f; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		zoomAmount = Camera.main.orthographicSize;
+
+		if (Input.GetAxis("Mouse ScrollWheel") > 0)
+		{
+			zoomAmount += zoomStep * Time.deltaTime;
+		}
+
+		if (Input.GetAxis("Mouse ScrollWheel") < 0)
+		{
+			zoomAmount -= zoomStep * Time.deltaTime;
+		}
+
+		Camera.main.orthographicSize = Vector2.Lerp(new Vector2(Camera.main.orthographicSize, 0.0f), new Vector2(zoomAmount, 0.0f), 10).x;
+	}
+}
