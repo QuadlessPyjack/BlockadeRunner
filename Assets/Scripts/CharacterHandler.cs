e8097283015e62cab01d969fb1ea26a286bf92c4
Bogdan Vitel <bogdan.vitel@outlook.com>
2017-01-22 00:17:30 +0200

    Added player controller script

--- Assets/Scripts/CharacterHandler.cs
+++ Assets/Scripts/CharacterHandler.cs
@@ -3,37 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterHandler : MonoBehaviour {
-    private GameObject cameraGO;
-    public float stepSize;
+    public float stepSize = 0.1f;
     // Use this for initialization
     void Start () {
-        cameraGO = gameObject.transform.Find("Camera").gameObject;
-        if(!cameraGO.GetComponent<Camera>())
-        {
-            Debug.LogError(gameObject.name + " has no Camera!");
-        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 reference = gameObject.transform.position;
+        Vector3 rotationReference = gameObject.transform.eulerAngles;
+        bool hasMoved = false;
         if (Input.GetButton("MOVE_DOWN"))
         {
-            gameObject.transform.Translate(new Vector3(0.0f, 0.0f, -stepSize));
+            //gameObject.transform.LookAt(new Vector3(reference.x, reference.y, -1000));
+            gameObject.transform.LookAt(new Vector3(-1000, 0.0f, 0.0f));
+            hasMoved = true;
         }
 
         if (Input.GetButton("MOVE_UP"))
         {
-            gameObject.transform.Translate(new Vector3(0.0f, 0.0f, stepSize));
+            gameObject.transform.LookAt(new Vector3(1000, 0.0f, 0.0f));
+            hasMoved = true;
+            //gameObject.transform.Translate(Vector3.forward);
         }
 
         if (Input.GetButton("MOVE_LEFT"))
         {
-            gameObject.transform.Translate(new Vector3(-stepSize, 0.0f, 0.0f));
+            gameObject.transform.LookAt(new Vector3(0.0f, 0.0f, 1000));
+            hasMoved = true;
+            //gameObject.transform.LookAt(new Vector3(-1000, reference.y, reference.z));
         }
 
         if (Input.GetButton("MOVE_RIGHT"))
         {
-            gameObject.transform.Translate(new Vector3(stepSize, 0.0f, 0.0f));
+            gameObject.transform.LookAt(new Vector3(0.0f, 0.0f, -1000));
+            hasMoved = true;
+            //gameObject.transform.LookAt(new Vector3(1000, reference.y, reference.z));
+        }
+
+        if(hasMoved)
+        {
+            gameObject.transform.Translate(Vector3.forward * stepSize);
+            //cameraGO.transform.rotation = cameraRotation;
+            hasMoved = false;
         }
     }
 }
