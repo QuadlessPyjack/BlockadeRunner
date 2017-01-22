1ef57e2746a8bb5d9aa732bda5aec34bf2374b20
Bogdan Vitel <bogdan.vitel@outlook.com>
2017-01-22 04:39:51 +0200

    got gamepad char control to work - finally

--- Assets/Scripts/CharacterHandler.cs
+++ Assets/Scripts/CharacterHandler.cs
@@ -4,68 +4,47 @@ using UnityEngine;
 
 public class CharacterHandler : MonoBehaviour {
     public float stepSize = 0.1f;
-    public float sensitivityX = 1.0f;
-    public float sensitivityY = 1.0f;
+    public float sensitivityLX = 1.0f;
+    public float sensitivityLY = 1.0f;
 
     // Use this for initialization
     void Start () {
 	}
+
+    private bool validateMovement(Vector3 input)
+    {
+        if (input.x > -0.1f && input.x < 0.1f)
+        {
+            return false;
+        }
+
+        if (input.z > -0.1f && input.z < 0.1f)
+        {
+            return false;
+        }
+
+        return true;
+    }
 	
+    private void handleMovement()
+    {
+        Vector3 NextDir = new Vector3(Input.GetAxis("LeftStickX"), 0, Input.GetAxis("LeftStickY"));
+        if (validateMovement(NextDir))
+        {
+            transform.rotation = Quaternion.LookRotation(NextDir * -1.0f);
+            return;
+        }
+
+        NextDir = new Vector3(Input.GetAxis("RightStickX"), 0, Input.GetAxis("RightStickY"));
+        if (validateMovement(NextDir))
+        {
+            transform.rotation = Quaternion.LookRotation(NextDir * -1.0f);
+            gameObject.transform.Translate(Vector3.forward * stepSize * 0.01f);
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
-        Vector3 reference = gameObject.transform.position;
-        Vector3 rotationReference = gameObject.transform.eulerAngles;
-        bool hasMoved = false;
-        float deltaX = 0.0f;
-        float deltaY = 0.0f;
-
-        deltaX = Input.GetAxis("Horizontal") * sensitivityX;
-        deltaY = Input.GetAxis("Vertical") * sensitivityY;
-
-        Debug.Log("HAxis: " + deltaX);
-        Debug.Log("VAxis: " + deltaY);
-        Vector3 targetRotation = transform.rotation.eulerAngles;
-        transform.Translate(Input.GetAxis("LeftStickX") * stepSize * Time.deltaTime, 0, Input.GetAxis("LeftStickY") * stepSize * Time.deltaTime);
-        transform.Rotate(Input.GetAxis("RightStickY") * Vector3.up * Time.deltaTime * stepSize);
-        //if (deltaY != 0)
-        //{
-        //    targetRotation = new Vector3(transform.rotation.eulerAngles.x, -deltaY, transform.rotation.eulerAngles.z);
-        //    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(targetRotation), .1f);
-        //}
-
-        //if (Input.GetButton("MOVE_DOWN"))
-        //{
-        //    //gameObject.transform.LookAt(new Vector3(reference.x, reference.y, -1000));
-        //    gameObject.transform.LookAt(new Vector3(-1000, 0.0f, 0.0f));
-        //    hasMoved = true;
-        //}
-
-        //if (Input.GetButton("MOVE_UP"))
-        //{
-        //    gameObject.transform.LookAt(new Vector3(1000, 0.0f, 0.0f));
-        //    hasMoved = true;
-        //    //gameObject.transform.Translate(Vector3.forward);
-        //}
-
-        //if (Input.GetButton("MOVE_LEFT"))
-        //{
-        //    gameObject.transform.LookAt(new Vector3(0.0f, 0.0f, 1000));
-        //    hasMoved = true;
-        //    //gameObject.transform.LookAt(new Vector3(-1000, reference.y, reference.z));
-        //}
-
-        //if (Input.GetButton("MOVE_RIGHT"))
-        //{
-        //    gameObject.transform.LookAt(new Vector3(0.0f, 0.0f, -1000));
-        //    hasMoved = true;
-        //    //gameObject.transform.LookAt(new Vector3(1000, reference.y, reference.z));
-        //}
-
-        if (hasMoved)
-        {
-            gameObject.transform.Translate(Vector3.forward * stepSize);
-            //cameraGO.transform.rotation = cameraRotation;
-            hasMoved = false;
-        }
+        handleMovement();
     }
 }
